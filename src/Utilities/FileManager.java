977df1f64580f03ba2858eaf240976d3eeecc928
TriickShotzShotz <matthew@mullinsfamily.ca>
2018-11-01 17:47:14 -0600

    Simple Parser

--- src/Utilities/FileManager.java
+++ src/Utilities/FileManager.java
@@ -8,73 +8,61 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FileManager {
-    private BufferedReader reader = null;
+    private BufferedReader configReader = null;
+    private BufferedReader inputReader = null;
     private BufferedWriter writer = null;
 
     private final Map<String, String> HEADERS = new HashMap<String, String>();
 
-    private final String SLOT_REGEX = "^[A-Z]{2}[\\s]*,[\\s]*[1-9]{1,2}:\\d{2}[\\s]*,[\\s]*\\d[\\s]*,[\\s]*\\d[\\s]*";
-    private final String COURSE_REGEX = "^[A-Z]{4}[\\s]*\\d{3}[\\s]*LEC[\\s]*\\d{2}$";
-    private final String LAB_REGEX = "^[A-Z]{4}[\\s]*\\d{3}[\\s]*[LEC[\\s]*\\d{2}[\\s]*]*(TUT|LAB)[\\s]*\\d{2}$";
+    private final String SLOT_REGEX = "[A-Z]{2}[\\s]*,[\\s]*\\d{1,2}:\\d{2}[\\s]*,[\\s]*\\d[\\s]*,[\\s]*\\d";
+    private final String COURSE_REGEX = "[A-Z]{4}[\\s]*\\d{3}[\\s]*LEC[\\s]*\\d{2}";
+    private final String LAB_REGEX = "[A-Z]{4}[\\s]*\\d{3}[\\s]*[LEC[\\s]*\\d{2}[\\s]*]*(TUT|LAB)[\\s]*\\d{2}";
+    private final String NOT_COMPATIBLE_REGEX = (COURSE_REGEX + "[\\s]*,[\\s]*" + COURSE_REGEX) + "|" +
+                                                (COURSE_REGEX + "[\\s]*,[\\s]*" + LAB_REGEX) + "|" +
+                                                (LAB_REGEX + "[\\s]*,[\\s]*" + COURSE_REGEX) + "|" +
+                                                (LAB_REGEX + "[\\s]*,[\\s]*" + LAB_REGEX) ;
+    private final String UNWANTED_REGEX = "[A-Z]{4}[\\s]*\\d{3}[\\s]*(LEC|LAB|TUT)[\\s]*\\d{2}[\\s]*,[\\s]*[A-Z]{2}[\\s]*,[\\s]*\\d{1,2}:\\d{2}";
+    private final String PREFERENCE_REGEX = "[A-Z]{2}[\\s]*,[\\s]*\\d{1,2}[\\s]*:\\d{2}[\\s]*,[\\s]*" + (COURSE_REGEX + "|" + LAB_REGEX) + "[\\s]*,[\\s]*\\d*";
+    private final String PAIR_REGEX =   (COURSE_REGEX + "[\\s]*,[\\s]*" + COURSE_REGEX) + "|" +
+                                        (COURSE_REGEX + "[\\s]*,[\\s]*" + LAB_REGEX) + "|" +
+                                        (LAB_REGEX + "[\\s]*,[\\s]*" + COURSE_REGEX) + "|" +
+                                        (LAB_REGEX + "[\\s]*,[\\s]*" + LAB_REGEX) ;
+    private final String PARTIAL_ASSIGNMENT_REGEX = (COURSE_REGEX + "|" + LAB_REGEX) + "[\\s]*,[\\s]*[A-Z]{2}[\\s]*,[\\s]*\\d{1,2}:\\d{2}";
 
-    private final String NOT_COMPATIBLE_REGEX =  COURSE_REGEX + "," + COURSE_REGEX + "|" +
-                                                    COURSE_REGEX + "," + LAB_REGEX + "|" +
-                                                    LAB_REGEX + "," + LAB_REGEX ;
     public FileManager(){
-
+        InitializeHeaders();
     }
 
     private void InitializeHeaders(){
-        HEADERS.put("Name:", "Name");
-        HEADERS.put("Course slots:", "Course Slots");
-        HEADERS.put("Lab slots:", "Lab Slots");
-        HEADERS.put("Courses:", "Courses");
-        HEADERS.put("Labs:", "Labs");
-        HEADERS.put("Not compatible:", "Not Compatible");
-        HEADERS.put("Unwanted:", "Unwanted");
-        HEADERS.put("Preference:", "Preference");
-        HEADERS.put("Pair:", "Pair");
-        HEADERS.put("Partial assignments:", "Partial Assignments");
+        //HEADERS.put("Name:", "Name");
+        HEADERS.put("Course slots:", SLOT_REGEX + "$");
+        HEADERS.put("Lab slots:", SLOT_REGEX + "$");
+        HEADERS.put("Courses:", COURSE_REGEX + "$");
+        HEADERS.put("Labs:", LAB_REGEX + "$");
+        HEADERS.put("Not compatible:", NOT_COMPATIBLE_REGEX + "$");
+        HEADERS.put("Unwanted:", UNWANTED_REGEX + "$");
+        HEADERS.put("Preferences:", PREFERENCE_REGEX + "$");
+        HEADERS.put("Pair:", PAIR_REGEX + "$");
+        HEADERS.put("Partial assignments:", PARTIAL_ASSIGNMENT_REGEX + "$");
     }
-
-    // slots_pattern = "^[A-Z]{2}[\\s]*,[\\s]*[1-9]{1,2}:\\d{2}[\\s]*,[\\s]*\\d[\\s]*,[\\s]*\\d[\\s]*";
-    // courses_pattern = "^[A-Z]{4}[\\s]*\\d{3}[\\s]*LEC[\\s]*\\d{2}[\\s]*"
-    // labs_pattern = "^[A-Z]{4}[\\s]*\\d{3}[\\s]*LEC[\\s]*\\d{2}[\\s]*(TUT|LAB)[\\s]*\\d{2}[\\s]*"
-
-
-    // not compatible = course pattern, course pattern
-    //                                | course pattern, lab Pattern | lab pattern, lab patter
-
-    // unwanted
 
     public void LoadConfig(String _configFileName){
         try{
             //Create file to read
-            File inputFile = new File(_configFileName);
-            //Create buffered reader
-            reader = new BufferedReader(new FileReader(inputFile));
-
-            Pattern coursesPattern = Pattern.compile(COURSE_REGEX);
-            Pattern labsPattern = Pattern.compile(LAB_REGEX);
-
-            //Line
+            File configFile = new File(_configFileName);
+            //Initialize buffered reader
+            configReader = new BufferedReader(new FileReader(configFile));
+            //Read lines
             String line;
-            while((line = reader.readLine()) != null){
-                Matcher courseMatcher = coursesPattern.matcher(line);
-                Matcher labMatcher = labsPattern.matcher(line);
-                while(courseMatcher.find()) {
-                    System.out.println("Course: " + line);
-                }
-                while(labMatcher.find()){
-                    System.out.println("Lab: " + line);
-                }
+            while((line = configReader.readLine()) != null){
+                LoadInput("src/InputFiles/" + line);
             }
         } catch(Exception e) {
             e.printStackTrace();
         } finally {
             try{
-                if(reader != null){
-                    reader.close();
+                if(configReader != null){
+                    configReader.close();
                 }
             } catch (Exception e){
                 e.printStackTrace();
@@ -82,8 +70,42 @@ public class FileManager {
         }
     }
 
-    private void LoadInput(){
-
+    private void LoadInput(String _inputFileName){
+        System.out.println("Loading File: " + _inputFileName);
+        try{
+            //Create file to read
+            File inputFile = new File(_inputFileName);
+            //Create buffered reader
+            inputReader = new BufferedReader(new FileReader(inputFile));
+            //Line
+            String line;
+            String regex = "";
+            String header = "";
+            Pattern pattern;
+            Matcher matcher;
+            while((line = inputReader.readLine()) != null){
+                if(HEADERS.containsKey(line)) {
+                    regex = HEADERS.get(line);
+                    header = line;
+                } else {
+                    pattern = Pattern.compile(regex);
+                    matcher = pattern.matcher(line);
+                    if(matcher.find()){
+                        System.out.println(header + " " + line);
+                    }
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(inputReader != null){
+                    inputReader.close();
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void WriteOutput(){
