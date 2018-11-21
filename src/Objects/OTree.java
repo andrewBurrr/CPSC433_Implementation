fd8a411787a202192b7ec1369a2d0ca006ddb1ce
qhnguyen <ikazuka36@hotmail.com>
2018-11-20 22:05:44 -0700

    Fix Parser bug

--- src/Objects/OTree.java
+++ src/Objects/OTree.java
@@ -6,10 +6,10 @@ import Structures.Slot;
 import java.util.LinkedList;
 import java.util.Map;
 
-public class Otree {
+public class OTree {
     private Prob root;
 
-    public Otree(Map<Course, Slot> rootData) {
+    public OTree(Map<Course, Slot> rootData) {
         root = new Prob(rootData, new Prob(), new LinkedList());
     }
 }
