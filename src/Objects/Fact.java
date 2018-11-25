6cbdfc9f2f14261cb83d6e746bf70b464ab0361e
trnewton <thomas.newton@ucalgary.ca>
2018-11-25 12:09:12 -0700

    Added constructor to fact that takes a parent and a new assignment.

--- src/Objects/Fact.java
+++ src/Objects/Fact.java
@@ -1,6 +1,8 @@
 package Objects;
 
+import Structures.Assignment;
 import Structures.Course;
+import Structures.Lecture;
 import Structures.Slot;
 
 import java.util.HashMap;
@@ -23,7 +25,34 @@ public class Fact {
      public Fact(HashMap<Course,Slot> schedule, int evaluation) {
          this.schedule = schedule;
          this.evaluation = evaluation;
+         // Something to generate numCourseSlot and numLabSlot
      }
+     
+     public Fact(Fact parent, Assignment newAssign){
+         this.schedule= new HashMap(parent.getScheduel());
+         this.numCourseSlot = new HashMap(parent.getNumCourses());
+         this.numLabSlot = new HashMap(parent.getNumLabs());
+         
+         this.schedule.put(newAssign.getCourse(),newAssign.getSlot());
+         Course newCourse = newAssign.getCourse();
+         Slot newSlot = newAssign.getSlot();
+         if(newCourse instanceof Lecture){
+             if(this.numCourseSlot.containsKey(newSlot)){
+                 Integer put = this.numCourseSlot.get(newSlot)+1;
+                 this.numCourseSlot.put(newSlot, put);
+             } else {
+                 this.numCourseSlot.put(newSlot, 1);
+             }
+         } else {
+             if(this.numLabSlot.containsKey(newSlot)){
+                 Integer put = this.numLabSlot.get(newSlot)+1;
+                 this.numLabSlot.put(newSlot, put);
+             } else {
+                 this.numLabSlot.put(newSlot, 1);
+             }
+         }
+     }
+     
 
      // needs
      public void setSchedule() {
