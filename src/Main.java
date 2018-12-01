07e3de7bc174d447493ab432b4adb6703c217a94
trnewton <thomas.newton@ucalgary.ca>
2018-12-01 16:31:48 -0700

    Edit

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,17 @@ import Parser.Reader;
 import SetBased.Fact;
 import SetBased.SetBased;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
+        // Weights to be changed according to config file or 
         float wMin = 1;
         float wPref = 1;
         float wPair = 1;
@@ -23,7 +26,13 @@ public class Main {
         File[] listOfInput;
         // Config 
         if(false){
-            
+            try {
+                Scanner fileReader = new Scanner(new File("config.txt"));
+                listOfInput = null;// Read config 
+                
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
         } else { // All files in InputFiles folder
             File folder = new File("src/InputFiles/");
             listOfInput= folder.listFiles();
@@ -32,6 +41,7 @@ public class Main {
         File error = new File("errors.txt");
         error.delete();
         
+        // Iterate over inputs, maybe combine this with the weights with non enhanced loop
         for(File test: listOfInput){
             if(/*test.toString().contains("6") &&*/ test.isFile() && !test.toString().contains("deptinst") && !test.toString().contains("/.") && !test.toString().contains("output")){
                 String inputFile = test.toString();
@@ -40,10 +50,11 @@ public class Main {
                     System.out.printf("Status: Reading File - %s\n",inputFile);
                     Reader reader = new Reader(inputFile, false);
                     OTreeModel otree;
-                    try {
+                    
+                    try { // Try intializing OTree
                         System.out.println("Status: Initiating Or Tree Model");
                         otree = new OTreeModel(reader);
-                    } catch(InvalidSchedulingException err){
+                    } catch(InvalidSchedulingException err){ // Catch Error in initialization and continue
                         System.out.printf("Name: %s\nStatus: UNSOLVED\n%s\n\n",reader.getName(),err.getMessage());
                         String outputFile = String.format("%s_output.txt", inputFile.replace(".txt", ""));
                         try (PrintWriter writer = new PrintWriter(new FileWriter(outputFile))) {
@@ -53,6 +64,7 @@ public class Main {
                     }
                     System.out.println("Status: Initiating Set Based Model");
                     System.out.println("Status: Reading Config");
+                    // Set weights for setbased
                     float[] weights = (new float[]{wMin,wPref,wPair,wSecD,p_CMin,p_LMin});
                     SetBased setBased = new SetBased(reader, otree, weights);
                     System.out.println("Status: Begining Set Based Search");
