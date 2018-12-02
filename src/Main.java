27ec942490cc053603ad7975c28072f9f383ce40
qhnguyen <ikazuka36@hotmail.com>
2018-12-01 23:12:01 -0700

    working on config

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,27 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        // Weights to be changed according to config file or 
+        // Weights to be changed according to config file or
         float wMin = 1;
         float wPref = 1;
         float wPair = 1;
         float wSecD = 1;
         float p_CMin = 1;
         float p_LMin = 1;
-        
-        
-        File[] listOfInput;
+
+
+        List<String> listOfInput = new LinkedList<>();
         float[][] weightsArr;
-        // Config 
+        // Config
 //        try {
 //            Scanner fileReader = new Scanner(new File("config.txt"));
 //            int numInputs = Integer.parseInt(fileReader.next());
@@ -39,24 +42,44 @@ public class Main {
 //            }
 //
 //        } catch (FileNotFoundException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-            File folder = new File("src/InputFiles/");
-            listOfInput= folder.listFiles();
+//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);.
+        try {
+            Scanner config = new Scanner(new File("src/config.txt")).useDelimiter("\\n");
+            while(config.hasNext()){
+                listOfInput.add(config.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 //        }
-        
         File error = new File("errors.txt");
         error.delete();
-        
+
         // Iterate over inputs, maybe combine this with the weights with non enhanced loop
-        for(File test: listOfInput){
-            if(/*test.toString().contains("6") &&*/ test.isFile() && !test.toString().contains("deptinst") && !test.toString().contains("/.") && !test.toString().contains("output")){
-                String inputFile = test.toString();
+        for(String line: listOfInput){
+//            if(/*test.toString().contains("6") &&*/ test.isFile() && !test.toString().contains("deptinst") && !test.toString().contains("/.") && !test.toString().contains("output")){
+            if(!line.equals("")){
+                String [] stringSplit = line.split(" ");
+                String inputFile = stringSplit[0];
+                wMin = Float.parseFloat(stringSplit[1]);
+                wPref = Float.parseFloat(stringSplit[2]);
+                wPair = Float.parseFloat(stringSplit[3]);
+                wSecD = Float.parseFloat(stringSplit[4]);
+                p_CMin = Float.parseFloat(stringSplit[5]);
+                p_LMin = Float.parseFloat(stringSplit[6]);
+                System.out.println(wMin);
+                System.out.println(wPref);
+                System.out.println(wPair);
+                System.out.println(wSecD);
+                System.out.println(p_CMin);
+                System.out.println(p_LMin);
                 try {
                     System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                     System.out.printf("Status: Reading File - %s\n",inputFile);
                     Reader reader = new Reader(inputFile, false);
+                    File file = new File(inputFile);
                     OTreeModel otree;
-                    
+
                     try { // Try intializing OTree
                         System.out.println("Status: Initiating Or Tree Model");
                         otree = new OTreeModel(reader);
@@ -75,10 +98,10 @@ public class Main {
                     SetBased setBased = new SetBased(reader, otree, weights);
                     System.out.println("Status: Begining Set Based Search");
                     Fact f = setBased.run();
-                    
+
                     if (f == null){
                         System.out.printf("\nName: %s\nStatus: UNSOLVED\nError: Infeasible Problem\n\n",reader.getName());
-                        
+
                         String outputFile = String.format("%s_output.txt", inputFile.replace(".txt", ""));
                         try (PrintWriter writer = new PrintWriter(new FileWriter(outputFile))) {
                             writer.write("Status: UNSOLVED\n");
@@ -109,7 +132,7 @@ public class Main {
                         Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
-            } 
+            }
         }
     }
 }
